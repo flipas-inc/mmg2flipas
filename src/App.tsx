4694ffba32ce376a4a3fc849d4b0580f268fdd36
Danil Sergeev <danil.kzn2003@gmail.com>
2025-01-28 17:24:27 +0100

    add: implementation for Sombra-only page(multiple styles)(✅)

--- src/App.tsx
+++ src/App.tsx
@@ -14,28 +14,48 @@
  * limitations under the License.
  */
 
-import { useRef, useState } from "react";
-import "./App.scss";
-import { LiveAPIProvider } from "./contexts/LiveAPIContext";
-import SidePanel from "./components/side-panel/SidePanel";
-import { Altair } from "./components/altair/Altair";
-import ControlTray from "./components/control-tray/ControlTray";
-import cn from "classnames";
+import { useRef, useState } from 'react'
+import './App.scss'
+import { LiveAPIProvider } from './contexts/LiveAPIContext'
+import SidePanel from './components/side-panel/SidePanel'
+import { Altair } from './components/altair/Altair'
+import ControlTray from './components/control-tray/ControlTray'
+import cn from 'classnames'
+import { Button, Container, Stack, Switch } from '@mui/material'
 
-const API_KEY = process.env.REACT_APP_GEMINI_API_KEY as string;
-if (typeof API_KEY !== "string") {
-  throw new Error("set REACT_APP_GEMINI_APIK_KEY in .env");
+const API_KEY = process.env.REACT_APP_GEMINI_API_KEY as string
+if (typeof API_KEY !== 'string') {
+  throw new Error('set REACT_APP_GEMINI_APIK_KEY in .env')
 }
 
-const host = "generativelanguage.googleapis.com";
-const uri = `wss://${host}/ws/google.ai.generativelanguage.v1alpha.GenerativeService.BidiGenerateContent`;
+const host = 'generativelanguage.googleapis.com'
+const uri = `wss://${host}/ws/google.ai.generativelanguage.v1alpha.GenerativeService.BidiGenerateContent`
 
 function App() {
   // this video reference is used for displaying the active stream, whether that is the webcam or screen capture
   // feel free to style as you see fit
-  const videoRef = useRef<HTMLVideoElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null)
   // either the screen capture, the video or null, if null we hide it
-  const [videoStream, setVideoStream] = useState<MediaStream | null>(null);
+  const [videoStream, setVideoStream] = useState<MediaStream | null>(null)
+  const [isShapeDefault, setIsShapeDefault] = useState(true)
+  const [isStyleFlipas, setIsStyleFlipas] = useState(true)
+  const [text, setText] = useState('AI')
+
+  const stylesColor = isStyleFlipas
+    ? {
+        color: 'rgba(255,255,255,0.7)',
+        backgroundColor: 'rgba(215, 232, 0, 1)',
+        '&:hover': {
+          backgroundColor: 'rgba(225, 225, 0, 1)',
+        },
+      }
+    : {
+        color: 'rgba(194,136,116,1)',
+        backgroundColor: 'rgba(255,255,255,1)',
+        '&:hover': {
+          backgroundColor: 'rgba(235,235,235,1)',
+        },
+      }
 
   return (
     <div className="App">
@@ -43,31 +63,75 @@ function App() {
         <div className="streaming-console">
           <SidePanel />
           <main>
-            <div className="main-app-area">
-              {/* APP goes here */}
-              <Altair />
-              <video
-                className={cn("stream", {
-                  hidden: !videoRef.current || !videoStream,
-                })}
-                ref={videoRef}
-                autoPlay
-                playsInline
-              />
-            </div>
-
-            <ControlTray
-              videoRef={videoRef}
-              supportsVideo={true}
-              onVideoStreamChange={setVideoStream}
+            <Container
+              disableGutters
+              sx={{ width: '100%', height: '100%', backgroundColor: 'green' }}
             >
-              {/* put your own buttons here */}
-            </ControlTray>
+              <div className="main-app-area">
+                {/* APP goes here */}
+                <Altair />
+                <video
+                  className={cn('stream', {
+                    hidden: !videoRef.current || !videoStream,
+                  })}
+                  ref={videoRef}
+                  autoPlay
+                  playsInline
+                />
+              </div>
+
+              <Stack
+                sx={{
+                  height: '100%',
+                  width: '100%',
+                  justifyContent: 'end',
+                  alignItems: 'center',
+                  flexDirection: 'column',
+                  backgroundColor: isStyleFlipas
+                    ? 'rgba(0,0,0,1)'
+                    : 'rgba(186,20,37,1)',
+                }}
+              >
+                {/* </ControlTray> */}
+
+                <Stack
+                  sx={{
+                    backgroundColor: 'gray',
+                    borderRadius: 10,
+                    padding: 1,
+                    margin: 2,
+                    flexDirection: 'row',
+                  }}
+                >
+                  <Switch
+                    onClick={() => {
+                      setIsShapeDefault(!isShapeDefault)
+                    }}
+                    color="default"
+                  />
+                  <Switch
+                    onClick={() => {
+                      setIsStyleFlipas(!isStyleFlipas)
+                    }}
+                    color="default"
+                  />
+                </Stack>
+
+                <ControlTray
+                  videoRef={videoRef}
+                  supportsVideo={true}
+                  onVideoStreamChange={setVideoStream}
+                  styles={stylesColor}
+                  isShapeDefault={isShapeDefault}
+                  isStyleFlipas={isStyleFlipas}
+                />
+              </Stack>
+            </Container>
           </main>
         </div>
       </LiveAPIProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/control-tray/ControlTray.tsx
+++ src/components/control-tray/ControlTray.tsx
@@ -14,31 +14,35 @@
  * limitations under the License.
  */
 
-import cn from "classnames";
+import cn from 'classnames'
 
-import { memo, ReactNode, RefObject, useEffect, useRef, useState } from "react";
-import { useLiveAPIContext } from "../../contexts/LiveAPIContext";
-import { UseMediaStreamResult } from "../../hooks/use-media-stream-mux";
-import { useScreenCapture } from "../../hooks/use-screen-capture";
-import { useWebcam } from "../../hooks/use-webcam";
-import { AudioRecorder } from "../../lib/audio-recorder";
-import AudioPulse from "../audio-pulse/AudioPulse";
-import "./control-tray.scss";
+import { memo, ReactNode, RefObject, useEffect, useRef, useState } from 'react'
+import { useLiveAPIContext } from '../../contexts/LiveAPIContext'
+import { UseMediaStreamResult } from '../../hooks/use-media-stream-mux'
+import { useScreenCapture } from '../../hooks/use-screen-capture'
+import { useWebcam } from '../../hooks/use-webcam'
+import { AudioRecorder } from '../../lib/audio-recorder'
+import AudioPulse from '../audio-pulse/AudioPulse'
+import './control-tray.scss'
+import { Button } from '@mui/material'
 
 export type ControlTrayProps = {
-  videoRef: RefObject<HTMLVideoElement>;
-  children?: ReactNode;
-  supportsVideo: boolean;
-  onVideoStreamChange?: (stream: MediaStream | null) => void;
-};
+  videoRef: RefObject<HTMLVideoElement>
+  children?: ReactNode
+  supportsVideo: boolean
+  onVideoStreamChange?: (stream: MediaStream | null) => void
+  styles?: object
+  isShapeDefault: boolean
+  isStyleFlipas: boolean
+}
 
 type MediaStreamButtonProps = {
-  isStreaming: boolean;
-  onIcon: string;
-  offIcon: string;
-  start: () => Promise<any>;
-  stop: () => any;
-};
+  isStreaming: boolean
+  onIcon: string
+  offIcon: string
+  start: () => Promise<any>
+  stop: () => any
+}
 
 /**
  * button used for triggering webcam or screen-capture
@@ -53,115 +57,159 @@ const MediaStreamButton = memo(
       <button className="action-button" onClick={start}>
         <span className="material-symbols-outlined">{offIcon}</span>
       </button>
-    ),
-);
+    )
+)
 
 function ControlTray({
   videoRef,
   children,
   onVideoStreamChange = () => {},
   supportsVideo,
+  styles,
+  isShapeDefault,
+  isStyleFlipas,
 }: ControlTrayProps) {
-  const videoStreams = [useWebcam(), useScreenCapture()];
+  const videoStreams = [useWebcam(), useScreenCapture()]
   const [activeVideoStream, setActiveVideoStream] =
-    useState<MediaStream | null>(null);
-  const [webcam, screenCapture] = videoStreams;
-  const [inVolume, setInVolume] = useState(0);
-  const [audioRecorder] = useState(() => new AudioRecorder());
-  const [muted, setMuted] = useState(false);
-  const renderCanvasRef = useRef<HTMLCanvasElement>(null);
-  const connectButtonRef = useRef<HTMLButtonElement>(null);
+    useState<MediaStream | null>(null)
+  const [webcam, screenCapture] = videoStreams
+  const [inVolume, setInVolume] = useState(0)
+  const [audioRecorder] = useState(() => new AudioRecorder())
+  const [muted, setMuted] = useState(false)
+  const renderCanvasRef = useRef<HTMLCanvasElement>(null)
+  const connectButtonRef = useRef<HTMLButtonElement>(null)
 
-  const { client, connected, connect, disconnect, volume } =
-    useLiveAPIContext();
+  const { client, connected, connect, disconnect, volume } = useLiveAPIContext()
 
   useEffect(() => {
     if (!connected && connectButtonRef.current) {
-      connectButtonRef.current.focus();
+      connectButtonRef.current.focus()
     }
-  }, [connected]);
+  }, [connected])
   useEffect(() => {
     document.documentElement.style.setProperty(
-      "--volume",
-      `${Math.max(5, Math.min(inVolume * 200, 8))}px`,
-    );
-  }, [inVolume]);
+      '--volume',
+      `${Math.max(5, Math.min(inVolume * 200, 8))}px`
+    )
+  }, [inVolume])
 
   useEffect(() => {
     const onData = (base64: string) => {
       client.sendRealtimeInput([
         {
-          mimeType: "audio/pcm;rate=16000",
+          mimeType: 'audio/pcm;rate=16000',
           data: base64,
         },
-      ]);
-    };
+      ])
+    }
     if (connected && !muted && audioRecorder) {
-      audioRecorder.on("data", onData).on("volume", setInVolume).start();
+      audioRecorder.on('data', onData).on('volume', setInVolume).start()
     } else {
-      audioRecorder.stop();
+      audioRecorder.stop()
     }
     return () => {
-      audioRecorder.off("data", onData).off("volume", setInVolume);
-    };
-  }, [connected, client, muted, audioRecorder]);
+      audioRecorder.off('data', onData).off('volume', setInVolume)
+    }
+  }, [connected, client, muted, audioRecorder])
 
   useEffect(() => {
     if (videoRef.current) {
-      videoRef.current.srcObject = activeVideoStream;
+      videoRef.current.srcObject = activeVideoStream
     }
 
-    let timeoutId = -1;
+    let timeoutId = -1
 
     function sendVideoFrame() {
-      const video = videoRef.current;
-      const canvas = renderCanvasRef.current;
+      const video = videoRef.current
+      const canvas = renderCanvasRef.current
 
       if (!video || !canvas) {
-        return;
+        return
       }
 
-      const ctx = canvas.getContext("2d")!;
-      canvas.width = video.videoWidth * 0.25;
-      canvas.height = video.videoHeight * 0.25;
+      const ctx = canvas.getContext('2d')!
+      canvas.width = video.videoWidth * 0.25
+      canvas.height = video.videoHeight * 0.25
       if (canvas.width + canvas.height > 0) {
-        ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-        const base64 = canvas.toDataURL("image/jpeg", 1.0);
-        const data = base64.slice(base64.indexOf(",") + 1, Infinity);
-        client.sendRealtimeInput([{ mimeType: "image/jpeg", data }]);
+        ctx.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height)
+        const base64 = canvas.toDataURL('image/jpeg', 1.0)
+        const data = base64.slice(base64.indexOf(',') + 1, Infinity)
+        client.sendRealtimeInput([{ mimeType: 'image/jpeg', data }])
       }
       if (connected) {
-        timeoutId = window.setTimeout(sendVideoFrame, 1000 / 0.5);
+        timeoutId = window.setTimeout(sendVideoFrame, 1000 / 0.5)
       }
     }
     if (connected && activeVideoStream !== null) {
-      requestAnimationFrame(sendVideoFrame);
+      requestAnimationFrame(sendVideoFrame)
     }
     return () => {
-      clearTimeout(timeoutId);
-    };
-  }, [connected, activeVideoStream, client, videoRef]);
+      clearTimeout(timeoutId)
+    }
+  }, [connected, activeVideoStream, client, videoRef])
 
   //handler for swapping from one video-stream to the next
   const changeStreams = (next?: UseMediaStreamResult) => async () => {
     if (next) {
-      const mediaStream = await next.start();
-      setActiveVideoStream(mediaStream);
-      onVideoStreamChange(mediaStream);
+      const mediaStream = await next.start()
+      setActiveVideoStream(mediaStream)
+      onVideoStreamChange(mediaStream)
     } else {
-      setActiveVideoStream(null);
-      onVideoStreamChange(null);
+      setActiveVideoStream(null)
+      onVideoStreamChange(null)
     }
 
-    videoStreams.filter((msr) => msr !== next).forEach((msr) => msr.stop());
-  };
+    videoStreams.filter((msr) => msr !== next).forEach((msr) => msr.stop())
+  }
 
   return (
-    <section className="control-tray">
-      <canvas style={{ display: "none" }} ref={renderCanvasRef} />
-      <nav className={cn("actions-nav", { disabled: !connected })}>
+    <section
+      className="control-tray"
+      style={{
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <canvas style={{ display: 'none' }} ref={renderCanvasRef} />
+
+      <div className={cn('connection-container', { connected })}>
+        {!isShapeDefault ? (
+          <Button
+            sx={{
+              height: '140px',
+              width: '140px',
+              borderRadius: 50,
+              fontSize: '36px',
+              ...styles,
+            }}
+            onClick={connected ? disconnect : connect}
+            ref={connectButtonRef}
+          >
+            {'AI'}
+          </Button>
+        ) : (
+          <Button
+            sx={{
+              height: '140px',
+              width: '140px',
+              borderRadius: 50,
+              border: 5,
+              borderColor: isStyleFlipas ? 'yellowgreen' : 'white',
+              borderStyle: 'double',
+            }}
+            onClick={connected ? disconnect : connect}
+            ref={connectButtonRef}
+          ></Button>
+        )}
+        {/* </div> */}
+        <span className="text-indicator">Streaming</span>
+      </div>
+      <nav
+        className={cn('actions-nav', { disabled: !connected })}
+        style={{ alignSelf: 'end' }}
+      >
         <button
-          className={cn("action-button mic-button")}
+          className={cn('action-button mic-button')}
           onClick={() => setMuted(!muted)}
         >
           {!muted ? (
@@ -195,23 +243,8 @@ function ControlTray({
         )}
         {children}
       </nav>
-
-      <div className={cn("connection-container", { connected })}>
-        <div className="connection-button-container">
-          <button
-            ref={connectButtonRef}
-            className={cn("action-button connect-toggle", { connected })}
-            onClick={connected ? disconnect : connect}
-          >
-          <div className="logo-container">
-           <img className="logo" src="/flipas-logo.svg" alt=""/>
-          </div>
-          </button>
-        </div>
-        <span className="text-indicator">Streaming</span>
-      </div>
     </section>
-  );
+  )
 }
 
-export default memo(ControlTray);
+export default memo(ControlTray)
